26d5b7102299499d9716ac4b562849d9a5d46e23
Devputra <devputra.n@gmail.com>
2025-10-10 02:25:05 +0530

    Replace Vercel favicon with Learnamyte logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LEARNAMYTE",
-  description: "Created by Appobang",
+  description: "Product of Appobang",
 };
 
 export default function RootLayout({
